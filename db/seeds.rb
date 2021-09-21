a79ab89c4c7d91fb44126346feafd5bb99bfe0c9
Erica Winne <erica.winne@powerhrg.com>
2021-09-21 16:51:13 -0400

    url helper lab... difficult/done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.create(first_name: 'Erica', last_name: 'Winne')
+Student.create(first_name: 'Benny', last_name: 'Dog')
